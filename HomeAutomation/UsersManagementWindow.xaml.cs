654a8d9a39b5ae02d8409df7627f322d92237339
idamou <othmane.idamou@emsi-edu.ma>
2022-12-21 22:55:21 +0100

    user management add button only * Not completed !!!!

--- HomeAutomation/UsersManagementWindow.xaml.cs
+++ HomeAutomation/UsersManagementWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using HomeAutomation.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -21,6 +22,18 @@ namespace HomeAutomation
     {
         private AuthWindow authWindow;
         private Person person;
+        private void fillUserListView()
+        {
+            UserListView.ItemsSource = new AppDbContext().Users.OrderByDescending(r => r.Id).ToList();
+            using AppDbContext dbContext = new AppDbContext();
+            UserListView.ItemsSource = dbContext.Users.ToList();
+        }
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            UserListView.Items.Clear();
+            fillUserListView();
+        }
+
 
         public UsersManagementWindow(AuthWindow authWindow, Person person)
         {
@@ -51,5 +64,55 @@ namespace HomeAutomation
             Close();
             authWindow.Show();
         }
+
+
+
+        private void ListView_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (UserListView.SelectedItems.Count > 0)
+            {
+
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (LoginBox.Text.Length == 0 || PasswordBox.Text.Length == 0 || FirstNameBox.Text.Length == 0 || LastNameBox.Text.Length == 0
+                || CinBox.Text.Length == 0 || BirthDayBox.Text.Length == 0 || PhoneBox.Text.Length == 0)
+            {
+                MessageBox.Show("Pleas fill all fields", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+            using AppDbContext dbContext = new AppDbContext();
+            if (dbContext.Users.Count(c => c.NIC == CinBox.Text) > 0)
+            {
+                MessageBox.Show("this CIN already exist", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+
+            dbContext.Users.Add(new User
+            {
+                Login = LoginBox.Text,
+                Password = PasswordBox.Text,
+                FirstName = FirstNameBox.Text,
+                LastName = LastNameBox.Text,
+                NIC = CinBox.Text,
+                PhoneNumber = PhoneBox.Text,
+            });
+            dbContext.SaveChanges();
+
+            LoginBox.Clear();
+            PasswordBox.Clear();
+            CinBox.Clear();
+            FirstNameBox.Clear();
+            LastNameBox.Clear();
+            PhoneBox.Clear();
+            BirthDayBox.Clear();
+            fillUserListView();
+
+
+        }
+
+        
     }
 }
